32600d045bd56521d89608b366640ff2f2e25820
Khusanbek Gafurov <x.gafurov@gmail.com>
2024-10-13 20:46:57 +0500

    Delete week 5 directory

--- week 5/CalculatorConsoleApp.java
+++
@@ -1,9 +0,0 @@
-
-public class CalculatorConsoleApp {
-
-    public static void main(String[] args) {
-
-        CalculatorRunner calculatorRunner = new CalculatorRunner();
-        calculatorRunner.run();
-    }
-}
